0325816c029f7d0a0fda583a688b07d142df5b45
nickfff-dev <hunchographics@gmail.com>
2022-01-28 00:12:49 +0300

    style base

---
+++ client/src/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Home extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+
+
+
+
+    render(){
+        return(
+            <div className="row">
+                <div className="col-md-4">
+                
+                </div>
+                <div className="col-md-6 col-md-offset-2">
+
+                </div>
+                
+            </div>
+        )
+    }
+}
