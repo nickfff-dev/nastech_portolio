244d5d6352c0db73189f1da0e1af8fe48478461c
nickfff-dev <hunchographics@gmail.com>
2022-01-28 00:18:04 +0300

    logo

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from './logo.png'
 
 class Home extends React.Component{
     constructor(props){
@@ -13,7 +14,8 @@ class Home extends React.Component{
         return(
             <div className="row">
                 <div className="col-md-4">
-                
+                    <img className="img=fluid"  src={Logo}/>
+                  
                 </div>
                 <div className="col-md-6 col-md-offset-2">
 
