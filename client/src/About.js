f6b07aa6a15fed893c3bd790fb407ab6bfe563ad
nickfff-dev <hunchographics@gmail.com>
2022-01-28 01:52:03 +0300

    good stuff

---
+++ client/src/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+
+class About extends React.Component {
+
+
+    render(){
+        return(
+                <div className="media col-sm-5 mt-4 mx-auto">
+  <div className="media-body mt-4 mx-auto ">
+    <p>I am a MERN stack developer with more
+than 5000 hours of building, maintaining and
+deploying single page
+applications, scripts, bots and scrapers. I adapt to any
+technology rapidly hence  flexibility. A passionate, diligent dev
+who seeks to learn continuously.</p>
+  </div>
+</div>
+            
+        )
+    }
+}
+
+export default About;
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from './logo.png'
+import About from "./About";
 
 class Home extends React.Component{
     constructor(props){
@@ -12,16 +13,30 @@ class Home extends React.Component{
 
     render(){
         return(
-            <div className="row">
-                <div className="col-md-4">
-                    <img className="img=fluid"  src={Logo}/>
-                  
-                </div>
-                <div className="col-md-6 col-md-offset-2">
+            <div className="row header container-fluid">
+              <div className="col-md-4">
+              <img className="img=fluid logo"  src={Logo}/>
+              </div>
+             
+                <div className="mt-3  col-md-4 title mx-auto">
+                
+                    <h1 className="bird">UI BIRD</h1>
+                    <h3 className="subheading">Full Stack Web Developer</h3>
 
                 </div>
-                
+
+                <div className="mt-3  col-md-4 mx-auto">
+                    <ul className="nav">
+                        <li className="pr-2 nav-link"> home</li>
+                        <li className="pr-2 nav-link"> about</li>
+                        <li className="pr-2 nav-link"> contact</li>
+                    </ul>
+                </div>
+
+               
             </div>
         )
     }
 }
+
+export default Home;
