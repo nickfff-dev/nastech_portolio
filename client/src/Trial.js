b6f103f172b567ee4e443c6b9f5b88318b14deb7
nickfff-dev <hunchographics@gmail.com>
2022-02-12 23:22:30 +0300

    cmt

--- client/src/Trial.js
+++ client/src/Trial.js
@@ -1,1 +1,13 @@
-rs
+import React, { Component } from 'react';
+
+class Trial extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Trial;
