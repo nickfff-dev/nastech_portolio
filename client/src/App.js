61e1fff8f50c514a1c9835ecab91ce6846f973af
nickfff-dev <hunchographics@gmail.com>
2022-03-06 23:29:36 +0300

    acc

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,61 @@ import About from './About';
 
 import Devicons from './Devicons'
 import Fronth from './Fronth';
-import { Parallax } from 'react-parallax';
-import image1 from "./79.jpg"
+import { Parallax, Background} from 'react-parallax';
+import image1 from "./sectionsbg.jpg"
 import image2 from "./1055.jpg"
+import Mania from './Mania';
 import image3 from "./855_programming_website.jpg"
 
 
 function App() {
   return (
     <div className="App">
-     <Home/>
-     <Fronth/>
-     
-      <About/>
-      <Devicons />
+   
       <Parallax
         bgImage={image1}
         bgImageAlt="image1"
+        strength={500}
+        className="container-fluid"
         
       
       >
+         <div style={{ height: "500px" }}>
+         <Mania/>
+        </div>
+      </Parallax>
+      <h2>| | |</h2>
+      <Parallax
+        bgImage={image2}
+        bgImageAlt="image2"
+        strength={500}
+        
+      
+      >
+        <div style={{ height: "500px" }}>
+          <About/>
+        </div>
+        
         
       </Parallax>
+      <h2>| | |</h2>
+      <Parallax
+        bgImage={image3}
+        bgImageAlt="image3"
+        strength={500}
+        
+      
+      > 
+        <div style={{ height: "500px" }} className="row">
+        
+          <About/>
+       
+        </div>
+        
+      </Parallax>
+
+
+      
     
       
       
